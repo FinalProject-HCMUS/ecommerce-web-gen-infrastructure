eefd20aa887b89ba802667f25019ea97e2070183
hoangtu47 <haquocbao607@gmail.com>
2025-04-10 00:41:24 +0700

    fix typo

--- infrastructure/terraform/7-eks.tf
+++ infrastructure/terraform/7-eks.tf
@@ -194,7 +194,7 @@ resource "helm_release" "aws_lb_controller" {
 
 # Helm release for ecommerce-web-gen-chart from OCI-based registry, Docker Hub
 resource "helm_release" "ecommerce_web_gen" {
-  name       = "ecommerce_web_gen"
+  name       = "ecommerce-web-gen"
   repository = "oci://registry-1.docker.io/21120414"
   chart      = "ecommerce-web-gen-chart"
   version    = "0.1.0"  # Pinned to a stable version 
